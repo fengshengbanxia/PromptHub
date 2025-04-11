90773543291aeff89d208a7312c6e8eabeb567e8
fengshengbanxia <3536559792@qq.com>
2025-04-11 16:01:55 +0800

    点击提示词在新标签页显示，只显示该提示词的内容和操作
    点击提示词在新标签页显示，只显示该提示词的内容和操作

--- prompthub/src/App.jsx
+++ prompthub/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { PromptProvider } from './context/PromptContext'
 import Home from './components/Home'
+import PromptView from './components/PromptView'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/prompt/:id" element={<PromptView />} />
         </Routes>
       </div>
     </PromptProvider>
